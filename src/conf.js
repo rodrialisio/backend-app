285e70b8d1bbf9277c19ef65de3ef635f6f756c2
Rodrigo Alisio <rodrigotalisio@gmail.com>
2022-01-04 18:53:53 -0300

    segunda entrega (revisada): mongodb/fileSystem/firebase

--- src/conf.js
+++ src/conf.js
@@ -8,6 +8,6 @@ export default {
         baseUrl: "mongodb+srv://rodrialisio:asd456@cluster0.a2jas.mongodb.net/ecommerce?retryWrites=true&w=majority"
     },
     firebase: {
-
+        databaseUrl: "https://ecommerce-bb234.firebaseio.com"
     } 
 }
